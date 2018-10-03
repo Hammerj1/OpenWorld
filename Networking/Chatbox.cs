4c55fc8890cb05c93cb59041fb4d95e520ac6899
Hammerj1 <36744186+Hammerj1@users.noreply.github.com>
2018-10-03 12:13:13 -0600

    Add files via upload

---
+++ Networking/Chatbox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chatbox : MonoBehaviour {
+    public Text[] textBoxes = new Text[5];
+
+
+    public void DisplayMessage(string message)
+    {
+        for(int i = textBoxes.Length-1; i >= 0; i--)
+        {
+            if (i == 0)
+            {
+                textBoxes[i].text = message;
+                return;
+            }
+            textBoxes[i].text = textBoxes[i - 1].text;
+        }
+    }
+}
---
+++ Networking/Tools.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public static class Tools {
+    public static Vector3 RandomVector()
+    {
+        return new Vector3(Random.Range(0, 10), 5, Random.Range(0, 10));
+    }
+    public static string RandomString(int size)
+    {
+        StringBuilder builder = new StringBuilder();
+        char ch;
+        for (int i = 0; i < size; i++)
+        {
+            ch = System.Convert.ToChar(System.Convert.ToInt32(Mathf.Floor(Random.Range(97,122))));
+            builder.Append(ch);
+        }
+
+        return builder.ToString();
+    }
+    public static string CombineStrings(string[] msg)
+    {
+        return string.Join(" ", msg);
+    }
+
+}
